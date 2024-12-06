e06d42b1a4346a92422f0c8c94201d9a32b368d9
Brandon <brandong9@sky.com>
2024-12-06 00:06:41 +0000

    Categories

--- src/routes/endpoints.js
+++ src/routes/endpoints.js
@@ -13,11 +13,37 @@ const router = Router();
 /* Using CSS Styles */
 router.use('/assets', static(join(__dirname, '../assets')))
 
+
 /* 
-TITLE: Home Page
+TITLE: Action Comics Page (Home)
+URL: http://localhost:3000/
 */
 router.get('/', async (request, response) => {
-    response.send(await renderEjsFile('home.ejs', { userSearch: request.query, dataFile }));
+    response.send(await renderEjsFile('home.ejs', { userSearch: request.query, dataFile, sort: 'action' }));
+});
+
+/* 
+TITLE: Action Comics Page
+URL: http://localhost:3000/action
+*/
+router.get('/action', async (request, response) => {
+    response.send(await renderEjsFile('home.ejs', { userSearch: request.query, dataFile, sort: 'action' }));
+});
+
+/* 
+TITLE: Fighting Comics Page
+URL: http://localhost:3000/fighting
+*/
+router.get('/fighting', async (request, response) => {
+    response.send(await renderEjsFile('home.ejs', { userSearch: request.query, dataFile, sort: 'fighting' }));
+});
+
+/* 
+TITLE: Horror Comics Page
+URL: http://localhost:3000/horror
+*/
+router.get('/horror', async (request, response) => {
+    response.send(await renderEjsFile('home.ejs', { userSearch: request.query, dataFile, sort: 'horror' }));
 });
 
 /* Exporting Router to be used in server.js */
